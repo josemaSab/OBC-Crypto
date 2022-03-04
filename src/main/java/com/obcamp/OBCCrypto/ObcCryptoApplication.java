eb5cbaeaf7fc3d31a74854f84334786b7ed48315
josemaSab <j.saborido@josemasaborido.es>
2022-03-04 19:25:49 +0100

    Initial Commit

---
+++ src/main/java/com/obcamp/OBCCrypto/ObcCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.obcamp.OBCCrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObcCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObcCryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obcamp/OBCCrypto/ObcCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obcamp.OBCCrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObcCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
